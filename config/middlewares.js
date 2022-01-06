6dedd9df2fbdb6c37d41d3e51e27afd5faf44805
Lexfer <lexferramirez@gmail.com>
2022-01-06 11:50:43 -0400

    add cloudinary config

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,4 +1,31 @@
+// module.exports = [
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::logger',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
+
 module.exports = [
+  // ...
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
@@ -8,4 +35,5 @@ module.exports = [
   'strapi::body',
   'strapi::favicon',
   'strapi::public',
+  // ...
 ];
